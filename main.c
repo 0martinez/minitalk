1fe371acf50e5e84b2b34a01c35f65222369036d
TTR0NC0 <Oscarmr632@gmail.com>
2022-03-02 00:53:23 +0100

    first push, not much just a main. Starting learning about unix signals

---
+++ main.c
@@ -0,0 +1,11 @@
+
+
+#include "minitalk.h"
+
+int	main(int argc, char **argv)
+{
+	pid_t pid;
+
+	pid = getpid();
+	return (0);
+}
---
+++ minitalk.h
@@ -0,0 +1,11 @@
+
+
+#ifndef MINITALK_H
+#define MINITALH_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/types.h>
+
+#endif
